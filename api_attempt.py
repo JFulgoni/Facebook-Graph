b1fe4d291396c14782893f5fa98ee1f5c0bf8a57
Jetoff <johnflgn@gmail.com>
2016-06-21 20:19:56 -0400

    separated the api stuff into another class

---
+++ api_attempt.py
@@ -0,0 +1,21 @@
+__author__ = 'johnfulgoni'
+
+import facebook
+import httplib
+
+def start():
+    #https://developers.facebook.com/tools/access_token/
+
+    #permanent
+    app_token = "599501133559658|B-oew8OsyP3AOZwOQSewKZCQSvk"
+
+    #below has an expiration of ~1 hour
+    user_token = "EAAIhPjwbt2oBABEZB6aUDu5PPlEayO8bNvhxGcoAVANmyf4BedJLLLssFTEtZBW3WNvHP5H8x8ZBXzlffWu8iNPZBr0zYiJ3pmyqVMFug5nXRU7OLRiJOFCOyGYvRpiYrOkpRHdiHsx0Y6reBY2ROjKlmYSj7v5zZC8ZBNjItU1AZDZD"
+
+    graph = facebook.GraphAPI(user_token)
+    profile = graph.get_object("me")
+    friends = graph.get_connections("me", "friends")
+
+    friend_list = [friend['name'] for friend in friends['data']]
+
+    print friend_list #not working!
